c3fbd92cd83653966a157f4c7dc3acdca17e0a4b
jWalker <rickhill543@gmail.com>
2021-03-28 22:42:15 -0400

    setup handlebars and front end javascripting and css

---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+// logs user out of their session
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  // takes user to homepage
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
